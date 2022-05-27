5195c535e8af342c1b7e763ccd9a6ee2d22e094f
Ahmed Charfeddine <68869463+AhmedCharfeddine@users.noreply.github.com>
2022-05-27 23:15:16 +0200

    Time: 98 ms (33.63%), Space: 15.9 MB (20.60%) - LeetHub

---
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,43 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if root == None:
+            return None
+        d = {}
+        level = 0
+        queue = [root]
+        right_node = None
+        while queue:
+            for _ in range(len(queue)): # traverse nodes at current level
+                node = queue.pop(0)
+                
+                # code for set next
+                if node.right != None:
+                    node.right.next = right_node
+                    if node.left != None:
+                        node.left.next = node.right
+                        right_node = node.left
+                    else:
+                        right_node = node.right
+                elif node.left != None:
+                    node.left.next = right_node
+                    right_node = node.left
+                
+                # append queue
+                if node.right != None:
+                    queue.append(node.right)
+                if node.left != None:
+                    queue.append(node.left)
+            
+            right_node = None
+            
+        return root
