b5e75bcf13fe5d8a60d678e6da8d5ef9b1b432b2
Ahmed Charfeddine <68869463+AhmedCharfeddine@users.noreply.github.com>
2022-05-31 22:25:24 +0200

    Time: 82 ms (14.32%), Space: 13.7 MB (97.94%) - LeetHub

--- 763-partition-labels/763-partition-labels.py
+++ 763-partition-labels/763-partition-labels.py
@@ -3,10 +3,15 @@ class Solution:
         if len(s) == 1: return [1]
         res = []
         ptr1, ptr2, ptr3 = 0, len(s) - 1, -1
+        last_appearances = {}
         while ptr1 < len(s):
             ptr2 = len(s) - 1
-            while s[ptr1] != s[ptr2] and ptr1 <= ptr2:
-                ptr2 -= 1
+            if s[ptr1] not in last_appearances:
+                while s[ptr1] != s[ptr2] and ptr1 <= ptr2:
+                    ptr2 -= 1
+                last_appearances[s[ptr1]] = ptr2
+            else:
+                ptr2 = last_appearances[s[ptr1]]
             if ptr2 > ptr3:
                 ptr3 = ptr2
             if ptr1 == ptr3:
