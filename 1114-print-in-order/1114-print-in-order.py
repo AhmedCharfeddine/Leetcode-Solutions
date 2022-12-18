0f02e41fe38d5a3dd015b6be4f2c0e03c20c775d
Ahmed Charfeddine <68869463+AhmedCharfeddine@users.noreply.github.com>
2022-12-18 12:55:38 +0100

    Time: 38 ms (96.86%), Space: 14.4 MB (95.41%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.py
@@ -0,0 +1,31 @@
+from threading import Lock
+
+class Foo:
+    def __init__(self):
+        self.lock2, self.lock3 = Lock(), Lock()
+        self.lock2.acquire()
+        self.lock3.acquire()
+
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        
+        # printFirst() outputs "first". Do not change or remove this line.
+        printFirst()
+        
+        self.lock2.release()
+
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        self.lock2.acquire()    
+    
+        # printSecond() outputs "second". Do not change or remove this line.
+        printSecond()
+        
+        self.lock3.release()
+
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        self.lock3.acquire()
+        
+        # printThird() outputs "third". Do not change or remove this line.
+        printThird()
