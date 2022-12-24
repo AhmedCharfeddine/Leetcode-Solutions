d94947a507f0f5bae3dc1233ca419679de3e6cdd
Ahmed Charfeddine <68869463+AhmedCharfeddine@users.noreply.github.com>
2022-12-24 13:40:18 +0100

    Time: 54 ms (70.32%), Space: 15.2 MB (33.65%) - LeetHub

--- 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.py
+++ 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.py
@@ -7,17 +7,15 @@ class Solution:
         if n == 1:  return 1
         dp = []
         for _ in range(n):
-            dp.append([None]*3)
-        dp[-1] = [1, 1, 1]
-        dp[-2] = [2, 1, 1]
+            dp.append([None]*2)
+        dp[-1] = [1, 1]
+        dp[-2] = [2, 1]
         def dfs(i, state):
             if dp[i][state] is not None:
                 return dp[i][state]
             if state == 0:      # both vertical cells are empty
-                dp[i][state] = int((dfs(i+2, 0) + dfs(i+1, 0) + dfs(i+1, 1) + dfs(i+1, 2)) % (1e9+7))
-            elif state == 1:    # bottom vertical cell is empty
-                dp[i][state] = int((dfs(i+2, 0) + dfs(i+1, 2)) % (1e9+7))
-            else:               # top vertical cell is empty
+                dp[i][state] = int((dfs(i+2, 0) + dfs(i+1, 0) + 2*dfs(i+1, 1)) % (1e9+7))
+            elif state == 1:    # bottom OR top vertical cell is empty
                 dp[i][state] = int((dfs(i+2, 0) + dfs(i+1, 1)) % (1e9+7))
             return dp[i][state]
         dfs(0,0)
