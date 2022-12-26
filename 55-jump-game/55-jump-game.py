6c0e237ec58a65aae6b98325b4cd02d00437cf07
Ahmed Charfeddine <68869463+AhmedCharfeddine@users.noreply.github.com>
2022-12-26 11:47:44 +0100

    Time: 2345 ms (9.89%), Space: 15.2 MB (49.64%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,9 +1,8 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         if len(nums) == 1:  return True
-        closest_finish = len(nums) - 1
-        for i in range(len((nums))-2, -1, -1):
-            reach = i + nums[i]
-            if reach >= closest_finish:  
-                closest_finish = i
-        return True if closest_finish == 0 else False
+        dp = [False] * len(nums)
+        dp[-1] = True
+        for i in range(len(nums)-2, -1, -1):
+            dp[i] = any(dp[i:i+nums[i]+1])
+        return dp[0]
