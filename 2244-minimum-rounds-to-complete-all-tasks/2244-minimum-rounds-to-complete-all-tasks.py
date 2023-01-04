d22b2e5872f8d0e1a82e2fd1b9f233344ebf9b59
Ahmed Charfeddine <68869463+AhmedCharfeddine@users.noreply.github.com>
2023-01-04 14:40:36 +0100

    Time: 1894 ms (47.05%), Space: 28.3 MB (60.98%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
@@ -1,16 +1,7 @@
 class Solution:
     def minimumRounds(self, tasks: List[int]) -> int:
-        hm = Counter(tasks)
         res = 0
-        dp = [None, None, 1, 1, 2, 2, 2, 3]
-        for diff in hm.values():
+        for diff in Counter(tasks).values():
             if diff == 1:   return -1
-            while diff >= len(dp):
-                if len(dp) % 3 == 0:
-                    dp.append(dp[-3]+1)
-                elif len(dp) % 3 == 1:
-                    dp.append(dp[-1]+1)
-                else:
-                    dp.append(dp[-2]+1)
-            res += dp[diff]
+            res += diff // 3 + int(diff % 3 != 0)
         return res
