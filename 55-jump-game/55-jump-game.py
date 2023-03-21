ee31f8e043d956eb14f4096e1466ed80b5b6b123
Ahmed Charfeddine <68869463+AhmedCharfeddine@users.noreply.github.com>
2023-03-21 21:52:29 +0100

    Time: 3759 ms (12.37%), Space: 15.4 MB (15.99%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -4,7 +4,7 @@ class Solution:
         dp = [False] * len(nums)
         dp[-1] = True
         for i in range(len(nums)-2, -1, -1):
-            for j in range(min(len(nums)-1,i+nums[i]), i, -1):
+            for j in range(min(len(nums)-1, i+nums[i]), i, -1):
                 if dp[j]:
                     dp[i] = True
                     break
